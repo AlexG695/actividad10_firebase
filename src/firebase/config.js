d3a9bcb7cbf2afe2d5844aef0969fb0804c82644
AlexG695 <alexg695845@gmail.com>
2022-03-24 22:07:12 -0700

    Subida del proyecyo

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,13 +3,13 @@ import '@firebase/auth';
 import '@firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyD4KaJ-MArSxzaByKuzyaM7QU1rBTRwt78',
-  authDomain: 'actividad-10-utch.firebaseapp.com',
-  databaseURL: 'https://actividad-10-utch-default-rtdb.firebaseio.com',
-  projectId: 'actividad-10-utch',
-  storageBucket: 'actividad-10-utch.appspot.com',
-  messagingSenderId: '912457574451',
-  appId: '1:912457574451:web:27ad1971316a81d0595b46',
+  apiKey: "AIzaSyAyOVZ9XhfOnqAvHp-mhxsEPfm0lBd1IMI",
+  authDomain: "actividad-10-94e4f.firebaseapp.com",
+  databaseURL: "https://actividad-10-94e4f-default-rtdb.firebaseio.com",
+  projectId: "actividad-10-94e4f",
+  storageBucket: "actividad-10-94e4f.appspot.com",
+  messagingSenderId: "860741947962",
+  appId: "1:860741947962:web:8c665acd0e91ba1e825b03"
 };
 
 if (!firebase.apps.length) {
